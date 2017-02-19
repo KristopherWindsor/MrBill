00670825cd9774db1cb63958b42dea0b6945cbfa
Kristopher Windsor <kristopher@pinger.com>
2017-02-18 23:04:12 -0800

    FAQ updates

--- html/faq.php
+++ html/faq.php
@@ -14,13 +14,26 @@
     <p>Mr. Bill is here to help you track expenses! Send him a text each time you spend $$, and he will keep track.</p>
 
     <h2>How can I get a report of my expenses?</h2>
-    <p>Mr. Bill will text you a link to your reports. You can also text "report" to Mr. Bill, and he will give you a report immediately.</p>
+    <p>Mr. Bill will text you a link to your reports.
+        You can also text "report" to Mr. Bill, and he will give you a report immediately.</p>
+
+    <h2>Is my data safe &amp; secure?</h2>
+    <p>Yes. Your phone is the key to your data.
+        Data can only be accessed with links or access codes that are sent to your phone via text message.</p>
+
+    <h2>How can I delete my account and remove my data?</h2>
+    <p>Text the exact phrase "delete my data" to 408 722 6296. You will be asked to confirm before the data is purged.</p>
+
+    <h2>Can I make a feature request?</h2>
+    <p>Let us know what you'd like to see by texting 408 722 6296.</p>
+
+    <h2>Can I upload pictures of my receipts?</h2>
+    <p>We may add support for that in the future. Let us know you want this feature by texting 408 722 6296.</p>
 
     <h2>Can I automatically track expenses like my phone bill?</h2>
-    <p>We may add support for that in the future.</p>
+    <p>We may add support for that in the future. Let us know you want this feature by texting 408 722 6296.</p>
 
     <h2>Can I setup a budget or spending goal?</h2>
-    <p>We may add support for that in the future.</p>
+    <p>We may add support for that in the future. Let us know you want this feature by texting 408 722 6296.</p>
 </body>
 </html>
-
