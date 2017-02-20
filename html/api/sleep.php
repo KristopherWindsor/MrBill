def7444a9ac4ef56e559506e8b1bf7345e118e80
Kristopher Windsor <kristopher@pinger.com>
2017-02-20 12:50:04 -0800

    fix bug

--- html/api/sleep.php
+++ html/api/sleep.php
@@ -7,4 +7,4 @@ $sleep = min(4, max(0, (int) @$_GET['sleep']));
 if ($sleep) sleep($sleep);
 
 header('Content-Type: application/xml');
-readfile(__DIR__ . '/../assets/welcome2.xml');
+readfile(__DIR__ . '/../assets/' . $content . '.xml');
