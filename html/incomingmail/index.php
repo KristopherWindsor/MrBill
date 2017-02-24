df8ea487e6ed26bee9fe837a06d1b09d8b01271b
Kristopher Windsor <kristopher@pinger.com>
2017-02-23 20:45:03 -0800

    test 2

--- html/incomingmail/index.php
+++ html/incomingmail/index.php
@@ -5,5 +5,6 @@ require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 // No clue what I'm doing
 
 $raw = file_get_contents('php://input');
+$headers = json_encode(getallheaders());
 $key = 'mail' . time();
-(new \MrBill\Persistence\DataStore())->put($key, $raw);
+(new \MrBill\Persistence\DataStore())->put($key, $raw . $headers);
